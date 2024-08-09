0a7734f2edb77052882f4a974788d6ee0bd3a709
bilalalibindal <bilalalibindal@gmail.com>
2024-08-09 11:10:39 +0300

    Smart Contract

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const TonWeb = window.TonWeb;
+    const tonweb = new TonWeb();
+    const myContractAddress = "EQD4eA1SdQOivBbTczzElFmfiKu4SXNL4S29TReQwzzr_70k";
+
     const tonConnect = new TonConnectUI({
         buttonRootId: 'ton-connect-button',
         manifestUrl: 'https://gist.githubusercontent.com/bilalalibindal/f2ee64abaac175576c26e93977ef307b/raw/5387ba4c0b6115b1f41ccc710abffa60846a420f/gistfile1.txt',
@@ -6,7 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     tonConnect.onStatusChange(async (status) => {
         if (status === 'connected') {
-            console.log('Connected to wallet:', tonConnect.wallet);
+            const wallet = tonConnect.wallet;
+
+            // Akıllı kontratın durumunu okuma
+            const myContract = new tonweb.Contract({ address: myContractAddress });
+            const contractState = await myContract.getState();
+            console.log('Contract State:', contractState);
+
+            // Akıllı kontrata işlem gönderme
+            const myMethodCall = await myContract.methods.myMethod({}).send(wallet);
+            console.log('Method Call Result:', myMethodCall);
         } else if (status === 'disconnected') {
             console.log('Wallet disconnected');
         }
